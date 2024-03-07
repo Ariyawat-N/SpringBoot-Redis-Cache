ae6fbaa42c3ab868ee35f87d9cb61e49fd0664dd
ging <ariyawat@altecflex.com>
2024-03-07 23:36:14 +0700

    first commit

---
+++ spring-boot-cache/src/main/java/com/gingeiei/springbootcache/User.java
@@ -0,0 +1,20 @@
+package com.gingeiei.springbootcache;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@ToString
+public class User implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
---
+++ spring-boot-cache/src/main/java/com/gingeiei/springbootcache/UserRepository.java
@@ -0,0 +1,6 @@
+package com.gingeiei.springbootcache;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
